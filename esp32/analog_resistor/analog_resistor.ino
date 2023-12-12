774e74de91c137f0fe2888bf025164f563678820
Arka Barua <cheattheweb@gmail.com>
2023-12-12 20:34:31 +0600

    arduino code works on esp32

---
+++ esp32/analog_resistor/analog_resistor.ino
@@ -0,0 +1,16 @@
+
+#define PIN_A 36
+int sensor_value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_A, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor_value = analogRead(PIN_A);
+  Serial.println(sensor_value);
+  delay(100);
+} 
